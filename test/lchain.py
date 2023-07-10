6a332f034b2a8e1b85801be05e1d1c9be1a2586d
thg80 <2576126597@qq.com>
2023-07-10 17:42:51 +0800

    初步修复Langchain使用tools出错的问题

--- test/lchain.py
+++ test/lchain.py
@@ -17,45 +17,61 @@ os.environ['openai_api_base'] = config['Chatgpt']['url']
 os.environ['OPENAI_API_KEY'] = config['Chatgpt']['api-key']
 os.environ['SERPAPI_API_KEY'] = config['Chatgpt']['serpapi-key']
 
+total_coast = .0
+total_coin = 0
+ 
 chat = ChatOpenAI(temperature=config['Chatgpt']['temperature'],model_name=config['Chatgpt']['model'],max_tokens=config['Chatgpt']['MaxTokens'])
 tools = load_tools(["serpapi", "llm-math","openweathermap-api"], llm=chat,openweathermap_api_key = config['Chatgpt']['openweathermap-api-key'])
+
+history = ChatMessageHistory()
+#memory = ConversationBufferMemory()
+memory = ConversationSummaryBufferMemory(llm=chat,max_token_limit=256)
 
 prompt = ConversationalAgent.create_prompt(
     tools, 
     prefix= """Act as a friend and a helpful assistant. Reply as short you can and please reply in Chinese. You can use the following tools:""", 
-    suffix= """[Remember try not to use 'search(serpapi)' as much as possible!] [Remember respond briefly in Chinese, but use English when using tools.]
-Previous conversation: {chat_history}
-Question: {input}
+    suffix= """Begin! [Remember try not to use 'search(serpapi)' as much as possible!] [Remember respond briefly in Chinese, but use English when using tools.]
+
+Previous conversation history: {chat_history}
+
+Current conversation: {input}
 {agent_scratchpad}""", 
     input_variables=["input", "chat_history","agent_scratchpad"]
 )
 
-history = ChatMessageHistory()
-#memory = ConversationBufferMemory()
-memory = ConversationSummaryBufferMemory(llm=chat,max_token_limit=512)
+
 llm_chain = LLMChain(llm=chat,prompt = prompt,verbose=True)
 agent= ConversationalAgent(llm_chain=llm_chain , tools = tools)
-agent_executor = AgentExecutor.from_agent_and_tools(agent=agent , tools = tools,verbose=True)
+agent_executor = AgentExecutor.from_agent_and_tools(agent=agent , tools = tools,verbose=True,handle_parsing_errors="Check your output and make sure it conforms!",max_iterations=3)
 
 dicts = messages_to_dict(history.messages)
-
+memory.save_context({"input": "我叫小明，请在回复的时候加上我的名字"}, {"output": "OK"})
 
 def send_chatgpt_request(send_msg):
+    global total_coast, total_coin
+
     try:
         with get_openai_callback() as cb:
-            response = agent_executor.run(input = send_msg,chat_history = memory.buffer)
+            response = agent_executor.run(input = send_msg, chat_history = memory.load_memory_variables(memory.chat_memory.messages))
             #花费计算
-            print(f'coast {(math.floor((math.floor(cb.prompt_tokens * 3 / 8) + math.floor(cb.completion_tokens / 2))) * 0.4 ) + 1} coins, Spent prompt: {cb.prompt_tokens} tokens | completion: {cb.completion_tokens} tokens')
-    except Exception as e:
+            coast = cb.total_cost
+            total_coast += coast
+            coin = (math.floor((math.floor(cb.prompt_tokens * 3 / 8) + math.floor(cb.completion_tokens / 2))) * 0.4 ) + 1
+            total_coin += coin
+
+            print(f'coast {round(total_coast,3)}￥, coast coins {int(coin)} - total {int(total_coin)}, Spend prompt: {cb.prompt_tokens} tokens | completion: {cb.completion_tokens} tokens')
+    except ValueError as e:
             response = str(e)
             if response.startswith("Could not parse LLM output: `"):
                 response = response.removeprefix("Could not parse LLM output: `").removesuffix("`")
-                return response
+            elif response.startswith("Got error from SerpAPI: Google hasn't returned any results for this query."):
+                print("An error occurred:", response)
+                return None
             else:
                 raise Exception(str(e))
 
-    memory.chat_memory.add_user_message(message)
-    memory.chat_memory.add_ai_message(response)
+
+    memory.save_context({"input": message}, {"output": response})
     
     return response
 
